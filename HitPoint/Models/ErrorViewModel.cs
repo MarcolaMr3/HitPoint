de00de5d721bb57d38383b1954fd8ffa74d58691
marcos.farias <marcos.farias@BUE0102D18>
2022-11-28 21:31:31 -0300

    Adicionar arquivos de projeto.

---
+++ HitPoint/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HitPoint.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
