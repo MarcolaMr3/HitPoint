e973303debf42c8941764efc7ca745dcdd9d715d
Guto510 <gubressaninini@gmail.com>
2022-11-29 20:38:28 -0300

    filial, empresa

---
+++ HitPointBib/Entidades/Empresa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HitPointBib.Entidades
+{
+    internal class Empresa
+    {
+        public int ID { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string NickName { get; set; } = string.Empty;
+        public string AcessType { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+        private static List<Empresa> _Records = new List<Empresa>();
+        public static List<Empresa> QuerryAll()
+        {
+            return _Records;
+        }
+        public static void Create(Empresa empresa)
+        {
+            _Records.Add(empresa);
+        }
+    }
+}
---
+++ HitPointBib/Entidades/Filial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HitPointBib.Entidades
+{
+    internal class Filial
+    {
+        public int ID { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string NickName { get; set; } = string.Empty;
+        public string AcessType { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+        private static List<Filial> _Records = new List<Filial>();
+        public static List<Filial> QuerryAll()
+        {
+            return _Records;
+        }
+        public static void Create(Filial filial)
+        {
+            _Records.Add(filial);
+        }
+    }
+}
