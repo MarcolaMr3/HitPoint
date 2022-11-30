592927e5434c0a9ef1006de731a879cc541f4333
MarcolaMr3 <marcosrobertomrf@hotmail.com>
2022-11-29 21:48:25 -0300

    Layout

--- HitPointBib/DBInfo/DBInfo.cs
+++ HitPointBib/DBInfo/DBInfo.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System.Data.SqlClient;
 
-namespace HitPointBib.DBInfo
-{
-    internal class DBInfo
-    {
-    }
-}
+//namespace TechStudents.Utils.DataBase
+//{
+//    internal class DBInfo
+//    {
+//       
+//        public const string DBConnection = @"Data Source=BUE0102D18\SQLEXPRESS;Initial catalog=TechStudents_DB;User=sa;Password=Senac@2021;Trusted_Connection=True;";
+//       
+
+//        public static void TestarBanco()
+//        {
+//            using (var conn = new SqlConnection(DBConnection))
+//            {
+//                var cmd = new SqlCommand("SELECT * FROM ALUNOS", conn);
+//                conn.Open();
+
+//                using (SqlDataReader reader = cmd.ExecuteReader())
+//                {
+//                    while (reader.Read())
+//                    {
+//                        Console.WriteLine($"{reader[1]}");
+//                    }
+//                }
+//            }
+
+//            Console.ReadLine();
+//        }
+//    }
+//}
