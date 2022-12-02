532d2b659f7a8bdaf5f12a8fdc4b2553d59d6056
Feerraz <guilhermeferraz2000@gmail.com>
2022-12-01 21:32:08 -0300

    paposjdalidoiud

---
+++ HitPoint/Models/PontoModel.cs
@@ -0,0 +1,29 @@
+﻿using HitPoint.Utils.Database;
+using HitPoint.Utils.Entidades;
+using System.Data.SqlClient;
+
+namespace HitPoint.Models
+{
+    public class PontoModel
+    {
+        public int Funcionario { get; set; }
+
+
+        public PontoModel() { }
+        public PontoModel(Ponto ponto)
+        {
+            Funcionario = ponto.Funcionario;
+        }
+
+        public Ponto GerarMarcacao()
+        {
+            var result = new Ponto()
+            {
+                Funcionario = Funcionario,
+            };
+            return result;
+        }
+
+    }
+}
+
