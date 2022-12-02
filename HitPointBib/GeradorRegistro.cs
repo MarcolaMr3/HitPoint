e1fb1fdcfa98b9fcf81de457044a14785aab8a2b
MarcolaMr3 <marcosrobertomrf@hotmail.com>
2022-12-02 14:23:46 -0300

    Pontotxt

--- HitPointBib/GeradorRegistro.cs
+++ HitPointBib/GeradorRegistro.cs
@@ -8,5 +8,24 @@ namespace HitPoint.Utils
 {
     internal class GeradorRegistro
     {
+        static void Pontotxt()
+        {
+            {
+                // 1: Escreve um linha para o novo arquivo
+                using (StreamWriter writer = new StreamWriter("C:\\dados\\macoratti.txt", true))
+                {
+                    writer.WriteLine("Macoratti .net");
+                }
+                // 2: Anexa uma linha ao arquivo
+                using (StreamWriter writer = new StreamWriter(@"C:\dados\macoratti.txt", true))
+                {
+                    writer.WriteLine("Quase tudo para Visual Basic");
+                }
+            }
+        }
+
     }
+
+
+
 }
